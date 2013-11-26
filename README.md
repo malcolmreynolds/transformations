f13a38f8b7126bc9692217f6068a1051d11624b3
Malcolm Reynolds <malcolm.reynolds@gmail.com>
2013-11-26 19:01:39 +0000

    More fixes to setup.py / Readme.

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
+Homogeneous Transformation Matrices and Quaternions.
+
+A library for calculating 4x4 matrices for translating, rotating, reflecting,
+scaling, shearing, projecting, orthogonalizing, and superimposing arrays of
+3D homogeneous coordinates as well as for converting between rotation matrices,
+Euler angles, and quaternions. Also includes an Arcball control object and
+functions to decompose transformation matrices.
+
+
+=================================
+
 This is my mirror of Christoph Gohlke's transformations package, 
 maintained at this location: http://www.lfd.uci.edu/~gohlke/
 
--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
 from setuptools import setup, Extension
 import numpy
-setup(name='_transformations',
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='transformations',
       author='Christoph Gohlke',
+      description="Homogeneous Transformation Matrices and Quaternions.",
+      license='BSD',
+      version='2013.06.29',
       packages=['transformations'],
+      long_description=read('README.md'),
+      install_requires=['numpy', 'cython'],
       ext_modules=[Extension('transformations._transformations', ['transformations/transformations.c'],
                              include_dirs=[numpy.get_include()])])
