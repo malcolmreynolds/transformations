ff1f6effe1438d7cbd572b38637aad98585b7ca0
Malcolm Reynolds <malcolm.reynolds@gmail.com>
2013-11-26 17:07:51 +0000

    initial commit of code from Christopher Gohlke

---
+++ setup.py
@@ -0,0 +1,5 @@
+  from distutils.core import setup, Extension
+  import numpy
+  setup(name='_transformations',
+        ext_modules=[Extension('_transformations', ['transformations.c'],
+                               include_dirs=[numpy.get_include()])])
