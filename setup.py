c731406356a42bf37a4168b28e7f2782e1e57f0d
Malcolm Reynolds <malcolm.reynolds@gmail.com>
2013-11-26 17:37:51 +0000

    Actually install the cython module

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ import numpy
 setup(name='_transformations',
       author='Christoph Gohlke',
       modules=['transformations'],
-      ext_modules=[Extension('_transformations', ['transformations/transformations.c'],
+      ext_modules=[Extension('transformations._transformations', ['transformations/transformations.c'],
                              include_dirs=[numpy.get_include()])])
