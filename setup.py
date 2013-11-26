7e9f54f5e46a4c0871ddd4501e4053a805726d71
Malcolm Reynolds <malcolm.reynolds@gmail.com>
2013-11-26 17:55:32 +0000

    fixed setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from distutils.core import setup, Extension
 import numpy
 setup(name='_transformations',
       author='Christoph Gohlke',
-      modules=['transformations'],
+      packages=['transformations'],
       ext_modules=[Extension('transformations._transformations', ['transformations/transformations.c'],
                              include_dirs=[numpy.get_include()])])
